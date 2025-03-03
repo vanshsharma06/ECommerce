34794b6d15d5ee990aebe6fade33aeb18ccaf0d8
vanshsharma06 <sharmavansh932@gmail.com>
2025-03-03 16:35:57 +0530

    third change

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -46,16 +46,16 @@ export default function Header() {
           <img
             src={photo}
             alt="ClassyShop"
-            className="h-4 w-17 sm:h-6 sm:w-21 md:h-7 md:w-27 lg:h-8 lg:w-30"
+            className="h-6 w-20 sm:h-6 sm:w-21 md:h-7 md:w-27 lg:h-8 lg:w-30"
           />
         </div>
 
         {/* Search Bar */}
-        <div className="relative w-48 mx-2 sm:w-80 sm:mx-3 md:w-100 lg:w-160 lg:mx-5">
+        <div className="relative w-35 mx-2 sm:w-80 sm:mx-3 md:w-100 lg:w-160 lg:mx-5">
           <input
             type="text"
             placeholder="Search for products..."
-            className="w-full bg-gray-200 text-gray-700 px-2 text-[7px] py-2 rounded-[5px] focus:outline-none sm:text-[9px] md:py-2 lg:py-3 lg:text-[11px]"
+            className="w-full bg-gray-200 text-gray-700 px-2 text-[8px] py-2 rounded-[5px] focus:outline-none sm:text-[9px] md:py-2 lg:py-3 lg:text-[11px]"
           />
           <Search
             className="absolute right-2 top-2 md:top-2 lg:top-3 text-gray-500 cursor-pointer"
@@ -67,14 +67,14 @@ export default function Header() {
         <div className="flex items-center sm:space-x-3 gap-2 lg:space-x-5 lg:gap-[4px]">
           <span className="flex justify-center items-center space-x-1 text-gray-700 cursor-pointer lg:gap-[4px]">
             <a
-              className="hover:text-red-500 text-[8px] sm:text-[10px] md:text-[11px] lg:text-[13px]"
+              className="hover:text-red-500 text-[9px] sm:text-[10px] md:text-[11px] lg:text-[13px]"
               href="#"
             >
               Login
             </a>
             <p className="text-[12px]">|</p>
             <a
-              className="hover:text-red-500 text-[8px] sm:text-[10px] md:text-[11px] lg:text-[13px]"
+              className="hover:text-red-500 text-[9px] sm:text-[10px] md:text-[11px] lg:text-[13px]"
               href="#"
             >
               Register
@@ -88,7 +88,7 @@ export default function Header() {
                   className="text-gray-600"
                   sx={{
                     fontSize: {
-                      xs: "14px",
+                      xs: "16px",
                       sm: "18px",
                       md: "20px",
                       lg: "24px",
@@ -107,7 +107,7 @@ export default function Header() {
                   className="text-gray-600"
                   sx={{
                     fontSize: {
-                      xs: "14px",
+                      xs: "16px",
                       sm: "18px",
                       md: "20px",
                       lg: "24px",
@@ -126,7 +126,7 @@ export default function Header() {
                   className="text-gray-600"
                   sx={{
                     fontSize: {
-                      xs: "14px",
+                      xs: "16px",
                       sm: "18px",
                       md: "20px",
                       lg: "24px",
