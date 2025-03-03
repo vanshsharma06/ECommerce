4d22de32beda38963f11e162ea2657a708051b37
vanshsharma06 <sharmavansh932@gmail.com>
2025-03-03 19:11:11 +0530

    third change

--- src/Components/FreeShipping/FreeShipping.jsx
+++ src/Components/FreeShipping/FreeShipping.jsx
@@ -5,16 +5,16 @@ export default function FreeShipping() {
     <div className="container">
       <div className="flex items-center justify-between bg-white border border-red-400 rounded-lg m-4 p-3 px-5 sm:p-4 sm:px-7 lg:p-5 lg:px-8 w-full max-w-6xl mx-auto">
         <div className="flex items-center gap-2">
-          <LiaShippingFastSolid className="text-gray-700 text-[18px] md:text-[25px] lg:text-[27px]" />
-          <span className=" text-gray-800 text-[9px] md:text-[12px] lg:text-[15px]">
+          <LiaShippingFastSolid className="text-gray-700 text-[20px] md:text-[25px] lg:text-[27px]" />
+          <span className=" text-gray-800 text-[11px] md:text-[12px] lg:text-[15px]">
             FREE SHIPPING
           </span>
         </div>
-        <p className="text-gray-600 text-[9px] md:text-[10px] lg:text-[11px]">
+        <p className="text-gray-600 text-[10px] sm:text-[11px] md:text-[10px] lg:text-[11px]">
           Free Delivery Now On Your First Order and over{" "}
           <span className="font-semibold">$200</span>
         </p>
-        <span className="font-bold text-gray-700 text-[9px] md:text-[15px] lg:text-[15px]">
+        <span className="hidden font-bold text-gray-700 sm:block sm:text-[9px] md:text-[15px] lg:text-[15px]">
           - Only $200*
         </span>
       </div>
